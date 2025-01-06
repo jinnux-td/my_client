d22a55dd746db9c10b8db6def78b15ea2d747fd9
jinnux <jinnux@nexondv.com>
2025-01-06 10:33:19 +0700

    build(dev tool): add lint, prettier to source code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Gallery from "src/components/Gallery";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Gallery />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/components/Profile.tsx
@@ -0,0 +1,6 @@
+const avatar = "https://i.imgur.com/MK3eW3Am.jpg";
+const description = "Katherine Johnson";
+
+export default function Profile() {
+  return <img src={avatar} alt={description} />;
+}
